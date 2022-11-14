f9010ee702e93573ca63d9887cf75f9ebf73857d
Alex <aser@jooble.com>
2022-11-14 21:24:28 +0200

    v-0.0.7

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './convertMaskToPlaceholder';
 export * from './isValidate';
+export * from './isRegExp';
 export * from './conformToMask';
 export * from './getSelectionRange';
 export * from './types';
