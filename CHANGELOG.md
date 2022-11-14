8646c1b7824468b8f82459423de5edf7a055dfb5
Alex <aser@jooble.com>
2022-11-14 21:09:14 +0200

    v-0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.0.2
 
 - Create useInputTextMask 
+
+## 0.0.3
+
+- Added utils types
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 rm -rf ./dist &&
 concurrently "tsc --p tsconfig.production.json" \
   "dts-bundle-generator --o ./dist/index.d.ts --no-check --no-banner ./src/index.ts" &&
-node ./scripts/beforePublish.js
+node ./scripts/beforePublish.js &&
+eslint --fix './dist/**/*.ts'
